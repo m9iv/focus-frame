f0ab0fcb5e2bac494c869f78fa32f4b5be1f205c
m9iv <aethawalka@gmail.com>
2025-07-09 12:39:38 +0300

    Add switches to Tasks

--- src/layout/GeneralFocus/components/FocusTask/index.jsx
+++ src/layout/GeneralFocus/components/FocusTask/index.jsx
@@ -1,45 +1,20 @@
 // Radix Components
-import { Card, Flex, Text } from '@radix-ui/themes'
-import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons'
+import { Card, Flex, Switch, Text } from '@radix-ui/themes'
 // Styles
 import styles from './FocusTask.module.css'
 
 function FocusTask({ task }) {
   return (
-    <Card
-      className={`${styles.focusTask} ${task.isPlaned ? 'active' : ''}`}
-      key={task.id}>
+    <Card variant="surface" className={styles.focusTask} key={task.id}>
       <Flex justify="between" align="center">
         <Text as="div" size="2">
           {task.name}
         </Text>
 
-        {task.isPlaned ? (
-          <ChevronLeftIcon color="gray" />
-        ) : (
-          <ChevronRightIcon color="gray" />
-        )}
+        <Switch size="1" defaultChecked={task.isPlaned} />
       </Flex>
     </Card>
   )
 }
 
 export default FocusTask
-
-//  <Card
-//   variant="surface"
-//   className={`${styles.focusTask} ${task.isPlaned ? 'active' : ''}`}
-//   key={task.id}>
-//   <Flex justify="between" align="center">
-//     <Text as="div" size="2">
-//       {task.name}
-//     </Text>
-
-//     {task.isPlaned ? (
-//       <ChevronLeftIcon color="gray" />
-//     ) : (
-//       <ChevronRightIcon color="gray" />
-//     )}
-//   </Flex>
-
-// </Card>
